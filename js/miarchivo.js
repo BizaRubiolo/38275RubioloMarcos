3bad1419cd08bcb942c23b2cd907f12ec6a953fe
unknown <you@example.com>
2022-08-02 22:52:50 -0300

    Desafio listo

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,1 +1,12 @@
-alert("HOLA MUNDO")
+let valor = Number(prompt("App para sumar!! Ingrese su primer valor: "));
+let resultado = 0;
+do{
+    resultado = resultado + valor;
+    valor = Number(prompt("Ingrese el valor que desea sumar a "+resultado+ ". Para finalizar ingrese cero."));
+}while (valor != 0)
+alert("Usted a finalizado su suma. El resultado final es: "+ resultado);
+
+let repet = Number(prompt("App para desear Feliz Cumpleaños. Uno por cada año que cumpla. Ingrese su nueva edad: "));
+for (let i = 1 ; i <= repet ; i++){
+    alert("FELIZ CUMPLEAÑOS " + i + " !!!");
+};
