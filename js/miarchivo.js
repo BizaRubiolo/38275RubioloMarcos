06ecbef9be221d4d35b896e106ab706fd2817afc
unknown <you@example.com>
2022-08-23 20:37:51 -0300

    Desafio 5 - Interactuar con HTML

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,3 +1,21 @@
+//Uso de innerTaxt para titulo
+let titulo = document.getElementById("titulo")
+titulo.innerText = "DESAFIO 5 - INTERACTUANDO CON HTML"
+
+//Uso de innerHTML para agregar contenido a un contenedor
+let container = document.getElementById("contenedor")
+container.innerHTML = "<h2>SIMULADOR TIENDA HOT SALE</h2><p><b>Alumno:</b> Marcos A. Rubiolo<br><br><b>Comisión:</b>38275</p>"
+
+//Uso de innerHTML para agregar contenido a un titulo
+let bienvenida = document.getElementById("bienvenida")
+bienvenida.innerHTML = "BIENVENIDO A NUESTRO HOT SALE !!!\nVEA NUESTRAS OFERTAS !!!"
+
+//Uso de los 3 pasos para agregar nodos con append
+let despedida = document.createElement("h3")
+despedida.innerHTML = "MUCHAS GRACIAS POR SU COMPRA !!!"
+document.body.append(despedida)
+
+
 //Funcion menu de opciones Principal
 const menuPrincipal = () => Number(prompt("Ingrese la opcion que desea realizar: \n1 - Ver productos disponibles y realizar una compra. \n2 - Modificacion de stock (Administradores).\n0 - Salir de la tienda"))
 
@@ -9,7 +27,7 @@ while(valor !=1 && valor !=2 && valor !=0){
 } return valor
 }
 
-//Funcion descripciones de productos para mostrar
+//Funcion para mostrar descripciones en prompt
 function descripcionesProd(lista){
     let descripMenu = []
     for (let elemento of lista){
@@ -19,14 +37,11 @@ function descripcionesProd(lista){
     return muestra;
 };
 
-//Funcion menu de opciones productos
+//Funcion menu de opciones productos y formas de pago
 function menuProd(dato){
     let elegirOp = prompt(dato);
     return elegirOp
 };
-
-//Funcion menu de opciones Formas de pago
-const menuPago = () => Number(prompt("En cuantas cuotas desea abonar: \n1)  Contado - Descuento 10% \n3)  3 Cuotas fijas - 10% interes\n6)  6 Cuotas fijas - 20% interes \n9)  9 Cuotas fijas - 35% interes \n12)  12 Cuotas fijas - 50% interes"))
 
 //Funcion verificacion codigo Producto
 function checkProd(valor, maximo){
@@ -36,28 +51,12 @@ while(valor < 1 || valor > maximo){
 } return valor
 }
 
-//Funcion verificacion codigo Forma de Pago
+//Funcion verificacion codigo Forma de Pago (Queda pendiente que el while sea variable, lo toque y exploto todo)
 function checkPago(valor){
-while(valor !=1 && valor !=3 && valor !=6 && valor !=9 && valor !=12){
+while(valor !=1 && valor !=3 && valor !=6 && valor !=12){
     alert("El numero de cuotas es invalido. Ingrese una cantidad existente: ");
-    valor = menuPago()
+    valor = menuProd(descripcionesProd(listaFromasDePago));
 } return valor
-}
-
-//Funcion elige Forma de Pago
-function pagarProd(cuotas, precio){
-    switch(cuotas){
-        case 1:
-            return precio * 0.9;
-        case 3:
-            return precio * 1.1 / 3;
-        case 6:
-            return precio * 1.2 / 6;
-        case 9:
-            return precio * 1.35 / 9;
-        case 12:
-            return precio * 1.5 / 12;
-    }
 }
 
 //Constructor de Objeto Producto
@@ -69,7 +68,7 @@ class Producto{
         this.modelo = modelo.toUpperCase();
         this.precio = Number(precio)
     }
-    descripProd(){return this.id+". "+this.tipo+". "+this.marca+". "+this.modelo+". "+"$" + this.precio}
+    descripProd(){return this.id+". "+this.tipo+". "+this.marca+". "+this.modelo+". "+"$" + this.precio};
 }
 
 //Se crean algunos productos para mostrar en lista inicial y se pushean dentro del array de productos
@@ -77,56 +76,71 @@ const prod1 = new Producto(1, "televisor", "philips", 'gf 43"', 85400);
 const prod2 = new Producto(2, "celular", "samsung", 'A32', 52300);
 const prod3 = new Producto(3, "tablet", "alcatel", 'M 10"', 38000)
 const listaProductos = [];
-listaProductos.push(prod1);
-listaProductos.push(prod2);
-listaProductos.push(prod3);
+listaProductos.push(prod1, prod2,prod3);
 
-//Bienvenida y Menu de Opciones Principal
-alert("BIENVENIDO A NUESTRO HOT SALE !!!\nVEA NUESTRAS OFERTAS !!!");
+//Constructor de Objeto Formas de Pago.
+class FormasPago{
+    constructor(cant, descripcion, interes){
+        this.cant = Number(cant);
+        this.descripcion = descripcion.toLowerCase();
+        this.interes = Number(interes)
+    }
+    descripProd(){return this.cant+" "+this.descripcion+" "+this.interes+"%"};
+}
 
+//Se crean algunas forma de pago y se pushean a la lista.
+const pago1 = new FormasPago(1, "pago con descuento de", -10);
+const pago2 = new FormasPago(3, "cuotas con interes de", 20);
+const pago3 = new FormasPago(6, "cuotas con interes de", 35);
+const pago4 = new FormasPago(12, "cuotas con interes de", 50);
+const listaFromasDePago = [];
+listaFromasDePago.push(pago1, pago2, pago3, pago4);
+
+//Bienvenida anterior comentada y nueva
+//alert("BIENVENIDO A NUESTRO HOT SALE !!!\nVEA NUESTRAS OFERTAS !!!");
+
+let prodHTML = document.getElementById("prodHTML")
+
+for (const prod of listaProductos) {
+    let list = document.createElement("li")
+    list.innerHTML = `${prod.tipo} ${prod.marca} ${prod.modelo}  <b>$${prod.precio}</b>`
+    prodHTML.appendChild(list)
+}
+
+//Menu de Opciones Principal
 let opcionPrincipal = checkPrincipal(menuPrincipal());
 
 while(opcionPrincipal != 0){
     if (opcionPrincipal === 1){
 
         //Compra de los Productos. Genera un nuevo array (compraParcial) con el  Id de los productos comprados.
-        let compraProd = checkProd(menuProd(descripcionesProd(listaProductos)), listaProductos.length);
+        let compraProd;
+        let masProd;
         const compraParcial = [];
-        compraParcial.push(compraProd);
-
-        let masProd = prompt("Desea continuar comprando? Si / No: ").toLowerCase();
-
-        while(masProd == "si"){
-        compraProd = checkProd(menuProd(descripcionesProd(listaProductos)), listaProductos.length);
-        compraParcial.push(compraProd);
-        masProd = prompt("Desea continuar comprando? Si / No: ").toLowerCase();
-        };
+        do{
+            compraProd = checkProd(menuProd(descripcionesProd(listaProductos)), listaProductos.length);
+            compraParcial.push(compraProd);
+            masProd = prompt("Desea continuar comprando? Si / No: ").toLowerCase();
+        }while(masProd == "si");
 
         //Utilizando el array con Id de productos comprados, generamos uno nuevo con todas las propiedades de los mismos.
         const compraFinal = [];
-        let j = 0;
-        for (let i = 0; i < compraParcial.length; i++){
-            j = compraParcial[i] - 1;
-            compraFinal.push(listaProductos[j])
+        for (const idCompra of compraParcial) {
+            comProCompleto = listaProductos.find((el) => el.id == idCompra)
+            compraFinal.push(comProCompleto)
         };
         alert("DETALLE DE LA COMPRA \n" + descripcionesProd(compraFinal))
 
-        //Calcula el monto final
-        let precioTotal = 0;
-        for (const valor of compraFinal) {
-            precioTotal += valor.precio;
-        };
+        //Calcula el monto final de la compra
+        let saldoAnterior = 0;
+        const precioTotal = compraFinal.reduce((acc, elemento) => acc + elemento.precio, saldoAnterior);
         alert("EL MONTO TOTAL DE LA COMPRA ES: \n$" + precioTotal)
 
-        //Elegir forma de Pago
-        let cantCuotas = checkPago(menuPago());
-        let precioPago = pagarProd(cantCuotas, precioTotal).toFixed(2);
-
-        if(cantCuotas === 1){
-            alert("Su importe final es de $" + precioPago);
-        } else{
-            alert("Su importe final es de "+ cantCuotas + " cuotas de $" + precioPago);
-        }
+        // //Elegir forma de Pago y mostrar comprobante final
+        let cantCuotas = checkPago(menuProd(descripcionesProd(listaFromasDePago)));
+        const cuotaElegida = listaFromasDePago.find((el) => el.cant == cantCuotas)
+        let precioFinal = (precioTotal * ((cuotaElegida.interes / 100) + 1) / cuotaElegida.cant).toFixed(2);
+        alert("COMPROBANTE \nSu importe final es de " + cantCuotas +" pagos de " + "$"+ precioFinal)
 
         alert("MUCHAS GRACIAS POR SU COMPRA !!!")
         //Una vez realizada la compra no se le permite seguir comprando ni cargar productos.
