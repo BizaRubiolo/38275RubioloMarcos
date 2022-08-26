abd3b41e3145affcb948b255e95eb0da2a761f09
unknown <you@example.com>
2022-08-25 23:56:50 -0300

    Desafio 6 - Eventos

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,6 +1,60 @@
+//Funcion constructor de Objeto Producto
+function objetoProductos(){
+    class Producto{
+        constructor(id, tipo, marca, modelo, precio){
+            this.id = id;
+            this.tipo = tipo.toUpperCase();
+            this.marca = marca.toUpperCase();
+            this.modelo = modelo.toUpperCase();
+            this.precio = Number(precio)
+        }
+        descripProd(){return `- ${this.tipo} ${this.marca} ${this.modelo} $${this.precio}`};
+    }
+
+    //Se crean algunos productos para mostrar en lista inicial y se pushean dentro del array de productos
+    const prod1 = new Producto(1, "televisor", "philips", 'gf 43"', 85400);
+    const prod2 = new Producto(2, "celular", "samsung", 'A32', 52300);
+    const prod3 = new Producto(3, "tablet", "alcatel", 'M 10"', 38000)
+    const prod4 = new Producto(4, "smartwatch", "xiaomi", 'miBand"', 14540)
+    const listaProductos = [];
+    listaProductos.push(prod1, prod2,prod3,prod4);
+    return listaProductos
+}
+
+//Funcion constructor de Objeto Formas de Pago.
+function objetoFormasPago(){
+    class FormasPago{
+        constructor(cant, descripcion, interes){
+            this.cant = Number(cant);
+            this.descripcion = descripcion.toLowerCase();
+            this.interes = Number(interes)
+        }
+        descripProd(){return this.cant+" "+this.descripcion+" "+this.interes+"%"};
+    }
+
+    //Se crean algunas forma de pago y se pushean a la lista.
+    const pago1 = new FormasPago(1, "pago con descuento de", -10);
+    const pago2 = new FormasPago(3, "cuotas con interes de", 20);
+    const pago3 = new FormasPago(6, "cuotas con interes de", 35);
+    const pago4 = new FormasPago(12, "cuotas con interes de", 50);
+    const listaFromasDePago = [];
+    listaFromasDePago.push(pago1, pago2, pago3, pago4);
+    return listaFromasDePago
+}
+
+//Funcion Evento click en boton comprar de cada producto
+function clickCompra(){
+    let botonComprar = document.getElementsByClassName("botonComprar")
+    let compraFinal = []
+    for(let i = 0 ; i < botonComprar.length ; i++){
+        botonComprar[i].onclick = () => {compraFinal.push(i+1)}
+    }
+    return compraFinal
+}
+
 //Uso de innerTaxt para titulo
 let titulo = document.getElementById("titulo")
-titulo.innerText = "DESAFIO 5 - INTERACTUANDO CON HTML"
+titulo.innerText = "DESAFIO 6 - EVENTOS"
 
 //Uso de innerHTML para agregar contenido a un contenedor
 let container = document.getElementById("contenedor")
@@ -10,159 +64,84 @@ container.innerHTML = "<h2>SIMULADOR TIENDA HOT SALE</h2><p><b>Alumno:</b> Marco
 let bienvenida = document.getElementById("bienvenida")
 bienvenida.innerHTML = "BIENVENIDO A NUESTRO HOT SALE !!!\nVEA NUESTRAS OFERTAS !!!"
 
-//Uso de los 3 pasos para agregar nodos con append
-let despedida = document.createElement("h3")
-despedida.innerHTML = "MUCHAS GRACIAS POR SU COMPRA !!!"
-document.body.append(despedida)
+//Llamo al boton VISITE TIENDA para poder usarlo
+let ingresar = document.getElementById("ingresar")
+let administrar = document.getElementById("administrador")
 
+//Evento click en VISITE TIENDA ingresa a ver los productos
+ingresar.onclick = () => {
+    //Una vez que ingresa el boton VISITE TIENDA se desactiva
+    ingresar.disabled = true;
+    administrar.disabled = true;
 
-//Funcion menu de opciones Principal
-const menuPrincipal = () => Number(prompt("Ingrese la opcion que desea realizar: \n1 - Ver productos disponibles y realizar una compra. \n2 - Modificacion de stock (Administradores).\n0 - Salir de la tienda"))
-
-//Funcion verificacion opcion Menu Principal
-function checkPrincipal(valor){
-while(valor !=1 && valor !=2 && valor !=0){
-    alert("La opcion no existe. Ingrese una opcion existente: ");
-    valor = menuPrincipal()
-} return valor
-}
-
-//Funcion para mostrar descripciones en prompt
-function descripcionesProd(lista){
-    let descripMenu = []
-    for (let elemento of lista){
-        descripMenu.push(elemento.descripProd());
+    //Llamamos al contenedor div "Productos" para mostrarlos en DOM
+    let productosDiv = document.getElementById("productos");
+    const listaProductos = objetoProductos();
+// Iteramos el array de productos y creamos nodos hijos dentro del div "Productos"
+    for (const prod of listaProductos) {
+        let list = document.createElement("p")
+        list.innerHTML = `<p>${prod.descripProd()}</p><button class="botonComprar">COMPRAR</button>`
+        productosDiv.appendChild(list)
     }
-    let muestra = descripMenu.join("\n");
-    return muestra;
-};
 
-//Funcion menu de opciones productos y formas de pago
-function menuProd(dato){
-    let elegirOp = prompt(dato);
-    return elegirOp
-};
+//Traemos el array con los id de los productos comprados
+    let queCompro = clickCompra()
 
-//Funcion verificacion codigo Producto
-function checkProd(valor, maximo){
-while(valor < 1 || valor > maximo){
-  alert("El codigo ingresado es invalido. Ingrese un codigo existente: ");
-  valor = menuProd(descripcionesProd(listaProductos));
-} return valor
-}
+//Traigo el div "botonsalir" para crearle un boton que nos pérmita finalizar la compra
+    let salirTienda = document.getElementById("botonsalir")
+    let botonSalir = document.createElement("button")
+    botonSalir.innerHTML = "FINALIZAR COMPRA"
+    salirTienda.appendChild(botonSalir)
+//se escucha el evento con la opcion .addEventListener()
+    botonSalir.addEventListener("click", finalCompra)
 
-//Funcion verificacion codigo Forma de Pago (Queda pendiente que el while sea variable, lo toque y exploto todo)
-function checkPago(valor){
-while(valor !=1 && valor !=3 && valor !=6 && valor !=12){
-    alert("El numero de cuotas es invalido. Ingrese una cantidad existente: ");
-    valor = menuProd(descripcionesProd(listaFromasDePago));
-} return valor
-}
-
-//Constructor de Objeto Producto
-class Producto{
-    constructor(id, tipo, marca, modelo, precio){
-        this.id = id;
-        this.tipo = tipo.toUpperCase();
-        this.marca = marca.toUpperCase();
-        this.modelo = modelo.toUpperCase();
-        this.precio = Number(precio)
-    }
-    descripProd(){return this.id+". "+this.tipo+". "+this.marca+". "+this.modelo+". "+"$" + this.precio};
-}
-
-//Se crean algunos productos para mostrar en lista inicial y se pushean dentro del array de productos
-const prod1 = new Producto(1, "televisor", "philips", 'gf 43"', 85400);
-const prod2 = new Producto(2, "celular", "samsung", 'A32', 52300);
-const prod3 = new Producto(3, "tablet", "alcatel", 'M 10"', 38000)
-const listaProductos = [];
-listaProductos.push(prod1, prod2,prod3);
-
-//Constructor de Objeto Formas de Pago.
-class FormasPago{
-    constructor(cant, descripcion, interes){
-        this.cant = Number(cant);
-        this.descripcion = descripcion.toLowerCase();
-        this.interes = Number(interes)
-    }
-    descripProd(){return this.cant+" "+this.descripcion+" "+this.interes+"%"};
-}
-
-//Se crean algunas forma de pago y se pushean a la lista.
-const pago1 = new FormasPago(1, "pago con descuento de", -10);
-const pago2 = new FormasPago(3, "cuotas con interes de", 20);
-const pago3 = new FormasPago(6, "cuotas con interes de", 35);
-const pago4 = new FormasPago(12, "cuotas con interes de", 50);
-const listaFromasDePago = [];
-listaFromasDePago.push(pago1, pago2, pago3, pago4);
-
-//Bienvenida anterior comentada y nueva
-//alert("BIENVENIDO A NUESTRO HOT SALE !!!\nVEA NUESTRAS OFERTAS !!!");
-
-let prodHTML = document.getElementById("prodHTML")
-
-for (const prod of listaProductos) {
-    let list = document.createElement("li")
-    list.innerHTML = `${prod.tipo} ${prod.marca} ${prod.modelo}  <b>$${prod.precio}</b>`
-    prodHTML.appendChild(list)
-}
-
-//Menu de Opciones Principal
-let opcionPrincipal = checkPrincipal(menuPrincipal());
-
-while(opcionPrincipal != 0){
-    if (opcionPrincipal === 1){
-
-        //Compra de los Productos. Genera un nuevo array (compraParcial) con el  Id de los productos comprados.
-        let compraProd;
-        let masProd;
-        const compraParcial = [];
-        do{
-            compraProd = checkProd(menuProd(descripcionesProd(listaProductos)), listaProductos.length);
-            compraParcial.push(compraProd);
-            masProd = prompt("Desea continuar comprando? Si / No: ").toLowerCase();
-        }while(masProd == "si");
-
-        //Utilizando el array con Id de productos comprados, generamos uno nuevo con todas las propiedades de los mismos.
+//Funcion para trasformar el array de id comprados a un array con los productos comprados
+    function finalCompra(){
         const compraFinal = [];
-        for (const idCompra of compraParcial) {
-            comProCompleto = listaProductos.find((el) => el.id == idCompra)
+        for (const idCompra of queCompro) {
+            let comProCompleto = listaProductos.find((el) => el.id == idCompra)
             compraFinal.push(comProCompleto)
         };
-        alert("DETALLE DE LA COMPRA \n" + descripcionesProd(compraFinal))
 
-        //Calcula el monto final de la compra
-        let saldoAnterior = 0;
-        const precioTotal = compraFinal.reduce((acc, elemento) => acc + elemento.precio, saldoAnterior);
-        alert("EL MONTO TOTAL DE LA COMPRA ES: \n$" + precioTotal)
+//Calcula el monto final de la compra
+    let saldoAnterior = 0;
+    const precioTotal = compraFinal.reduce((acc, elemento) => acc + elemento.precio, saldoAnterior);
 
-        // //Elegir forma de Pago y mostrar comprobante final
-        let cantCuotas = checkPago(menuProd(descripcionesProd(listaFromasDePago)));
-        const cuotaElegida = listaFromasDePago.find((el) => el.cant == cantCuotas)
-        let precioFinal = (precioTotal * ((cuotaElegida.interes / 100) + 1) / cuotaElegida.cant).toFixed(2);
-        alert("COMPROBANTE \nSu importe final es de " + cantCuotas +" pagos de " + "$"+ precioFinal)
+//Llamamos al div "Muestra" para mostrar la compra en el DOM
+    let productosDetalle = document.getElementById("muestra");
+    productosDetalle.innerHTML = "<h3>DETALLE DE COMPRA</h3>"
+//Iteramos el array de productos y creamos nodos hijos dentro del div "Productos"
+        for (const prod of compraFinal) {
+            let detalle = document.createElement("p")
+            detalle.innerHTML = `<p>${prod.descripProd()}</p>`
+            productosDetalle.appendChild(detalle)
+        }
 
-        alert("MUCHAS GRACIAS POR SU COMPRA !!!")
-        //Una vez realizada la compra no se le permite seguir comprando ni cargar productos.
-        break
-    } 
-    // Opcion para agregar productos al stock.
-     else if (opcionPrincipal === 2) {
-        let cargarProd
-        do{
-            let tipo = prompt("Ingresa el tipo de producto")
-            let marca = prompt("Ingresa la marca del producto")
-            let modelo = prompt("Ingresa el modelo del producto")
-            let precio = prompt("Ingresa el precio del producto")
-            let id = listaProductos.length + 1;
-            listaProductos.push(new Producto(id, tipo, marca, modelo, precio));
-            cargarProd = prompt("Desea cargar otro producto? Si/No ").toLowerCase();
-        } while(cargarProd === "si");
+//Mostramos precio final    
+        let montoFinal = document.createElement("p")
+        montoFinal.innerHTML = `<b>Monto Final: $${precioTotal}</b>`
+        productosDetalle.appendChild(montoFinal)
+    }
 
-        alert("NUEVO STOCK DE LA TIENDA \n" + descripcionesProd(listaProductos));
-    } 
-    //Despues de la carga se le vuelve a mostrar el menu de opciones principal
-    opcionPrincipal = checkPrincipal(menuPrincipal());
-};
+}
 
-alert("MUCHAS GRACIAS POR SU VISITA!!\nLO ESPERAMOS PRONTO!!")
+//Ingreso al sector Administradores con boton administrador
+administrar.onclick = () => {
+    //Una vez que ingresa los botnes se desactivan
+    ingresar.disabled = true;
+    administrar.disabled = true;
+    
+    //Uso de innerHTML para agregar inputs de administracion
+let loginAdmin = document.getElementById("loginAdmin")
+loginAdmin.innerHTML ='<form action="" id="login"><input type="text" required placeholder="Usuario"><input type="password" required placeholder="Password"><input type="submit" value="Enviar"></form>'
+
+//Recibo los datos del login
+let rechazarLog = document.getElementById("login")
+rechazarLog.addEventListener("submit", datosForm)
+//Creo un alert sin sentido porque el resto del Programa se me rompio todo
+function datosForm(e){
+    e.preventDefault();
+    let datos = e.target
+    alert("Contraseña Incorrecta")
+}
+}
